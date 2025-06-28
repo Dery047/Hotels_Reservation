acc75f21a83457b04e6f4248f3719e1550881107
Santiago <vergarasantiago049@gmail.com>
2025-06-28 16:49:48 -0500

    adding documentation

--- reservations_app/serializers.py
+++ reservations_app/serializers.py
@@ -2,41 +2,48 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import *
 
+# Serializer for User model (read-only password)
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
-        fields = ['id', 'username', 'email']
+        model = User  # Model to serialize
+        fields = ['id', 'username', 'email']  # Fields to include in serialization
 
+# Serializer for registering new users
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
-        fields = ['username', 'email', 'password']
-        extra_kwargs = {'password': {'write_only': True}}
+        model = User  # Model to create
+        fields = ['username', 'email', 'password']  # Fields expected on registration
+        extra_kwargs = {'password': {'write_only': True}}  # Password won't be returned on GET
 
+    # Override create to use Django's create_user (hashes password)
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
 
+# Serializer for Flight model with all fields
 class FlightSerializer(serializers.ModelSerializer):
     class Meta:
         model = Flight
-        fields = '__all__'
+        fields = '__all__'  # Include all fields from Flight model
 
-# class FlightReservationSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = FlightReservation
-#         fields = '__all__'
-
+# Serializer for FlightReservation model with all fields
+class FlightReservationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FlightReservation
+        fields = '__all__'  
+# Serializer for Hotel model with all fields
 class HotelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Hotel
         fields = '__all__'
 
+# Serializer for Room model with all fields
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
         fields = '__all__'
 
-# class RoomReservationSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = RoomReservation
-#         fields = '__all__'
+# Serializer for RoomReservation model with all fields
+class RoomReservationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RoomReservation
+        fields = '__all__'
