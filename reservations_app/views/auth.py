50fc0900141da6ea8f64b1c6a80231f2596f8833
Santiago <vergarasantiago049@gmail.com>
2025-07-01 20:20:13 -0500

    Importations problems fixed

--- reservations_app/views/auth.py
+++ reservations_app/views/auth.py
@@ -1,14 +1,19 @@
 from rest_framework import generics
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework.permissions import AllowAny
-from ..serializers import RegisterSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from django.contrib.auth.models import User
+from ..serializers import RegisterSerializer
 
+# Users registration
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = [AllowAny]
     serializer_class = RegisterSerializer
 
+# Login - gives access and refresh token
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = TokenObtainPairSerializer
+
+TokenRefreshView = TokenRefreshView
+
