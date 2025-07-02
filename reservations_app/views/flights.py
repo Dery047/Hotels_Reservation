cc9d3becb4ba54eb998608c414f7457ff7e48d31
Santiago <vergarasantiago049@gmail.com>
2025-07-01 20:11:39 -0500

    adding flights.py view

--- reservations_app/views/flights.py
+++ reservations_app/views/flights.py
@@ -2,3 +2,15 @@ from rest_framework import generics, permissions
 from ..models import Flight, FlightReservation
 from ..serializers import FlightSerializer, FlightReservationSerializer
 
+class FlightListView(generics.ListAPIView):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+    permission_classes = [permissions.AllowAny]
+
+class ReserveFlightView(generics.CreateAPIView):
+    serializer_class = FlightReservationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+        
