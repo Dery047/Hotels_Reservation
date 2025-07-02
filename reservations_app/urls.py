8aa1e03d5633d49ba41e9546a1611213c0f9d181
Santiago <vergarasantiago049@gmail.com>
2025-07-01 20:49:24 -0500

    Add additional endpoints to urls.py and update settings

--- reservations_app/urls.py
+++ reservations_app/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
-from .views import auth
+from .views import auth, flights, hotels
 
 urlpatterns = [
-    # JWT Authentication
-    path('register/', auth.RegisterView.as_view(), name='register'),  # POST
-    path('token/', auth.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),  # POST
-    path('token/refresh/', auth.TokenRefreshView.as_view(), name='token_refresh'),  # POST
-]
+    # Auth
+    path('register/', auth.RegisterView.as_view(), name='register'),
+    path('token/', auth.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', auth.TokenRefreshView.as_view(), name='token_refresh'),
 
+    # Flights
+    path('flights/', flights.FlightListView.as_view(), name='flight_list'),
+    path('flights/reserve/', flights.ReserveFlightView.as_view(), name='reserve_flight'),
+
+    # Hotels
+    path('hotels/', hotels.HotelListView.as_view(), name='hotel_list'),
+    path('rooms/', hotels.RoomListView.as_view(), name='room_list'),
+    path('rooms/reserve/', hotels.ReserveRoomView.as_view(), name='reserve_room'),
+]
