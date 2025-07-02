807bf5ae715090582a3cca688127016352727ede
Santiago <vergarasantiago049@gmail.com>
2025-07-01 20:35:52 -0500

    adding requirements.txt and urls.py correction

--- reservations_app/urls.py
+++ reservations_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import auth
 
 urlpatterns = [
-    # Autenticación con JWT
+    # JWT Authentication
     path('register/', auth.RegisterView.as_view(), name='register'),  # POST
     path('token/', auth.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),  # POST
     path('token/refresh/', auth.TokenRefreshView.as_view(), name='token_refresh'),  # POST
