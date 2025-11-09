01c548413174dce7306176c02af534a87b336aec
Dery047 <vergarasantiago049@gmail.com>
2025-11-08 21:50:37 -0500

    agregando documentacion para api

--- fronthotels/src/api/flight.js
+++ fronthotels/src/api/flight.js
@@ -1,39 +1,39 @@
-const BASE_URL = "http://localhost:8000/api"; // ajusta si tu backend tiene otro path
+const BASE_URL = "http://localhost:8000/api";  //locla path
 
-export async function getFlights() {
+export async function getFlights() { // get list of all flights and return the data in json 
   const res = await fetch(`${BASE_URL}/flights/`);
   return res.json();
 }
 
-export async function getFlightDetail(id) {
+export async function getFlightDetail(id) {  
   const res = await fetch(`${BASE_URL}/flights/${id}/`);
   return res.json();
 }
 
-export async function createReservation(token, data) {
-  const res = await fetch(`${BASE_URL}/reserve/`, {
-    method: "POST",
+export async function createReservation(token, data) {  //recibe data y tokens  
+  const res = await fetch(`${BASE_URL}/reserve/`, { //http solicitud 
+    method: "POST", 
     headers: {
       "Content-Type": "application/json",
-      "Authorization": `Bearer ${token}`,
+      "Authorization": `Bearer ${token}`, //pasa el token jwt usando bearer 
     },
     body: JSON.stringify(data),
   });
   return res.json();
 }
 
-export async function getMyReservations(token) {
-  const res = await fetch(`${BASE_URL}/my-reservations/`, {
+export async function getMyReservations(token) { //requiere token para obtener los datos de reserva
+  const res = await fetch(`${BASE_URL}/my-reservations/`, { //apunta a ruta 
     headers: {
-      "Authorization": `Bearer ${token}`,
+      "Authorization": `Bearer ${token}`, 
     },
   });
   return res.json();
 }
 
-export async function cancelReservation(token, id) {
+export async function cancelReservation(token, id) { //id de reserva es requerido
   const res = await fetch(`${BASE_URL}/cancel/${id}/`, {
-    method: "DELETE",
+    method: "DELETE", //se elimina la reserva
     headers: {
       "Authorization": `Bearer ${token}`,
     },
