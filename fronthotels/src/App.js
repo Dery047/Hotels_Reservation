cb57e689ca8622487f42bcd7e010c88cf48ef753
Dery047 <vergarasantiago049@gmail.com>
2025-11-08 21:37:44 -0500

    add App.jsx main layout for flight reservation

--- fronthotels/src/App.js
+++ fronthotels/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import FlightList from "./components/FlightList";
+import FlightDetail from "./components/FlightDetail";
+import ReservationForm from "./components/ReservationForm";
+import MyReservations from "./components/MyReservations";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [selectedFlight, setSelectedFlight] = useState(null);
+  const [token] = useState("AQUI_TU_TOKEN_JWT_O_DE_PRUEBA");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>🛫 Sistema de Reservas de Vuelos</h1>
+      <FlightList onSelect={setSelectedFlight} />
+      <FlightDetail flightId={selectedFlight} />
+      {selectedFlight && <ReservationForm flightId={selectedFlight} token={token} />}
+      <MyReservations token={token} />
     </div>
   );
 }
-
-export default App;
